83641ec556a1b28785d87b2614e6faed964b2a42
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-10-14 13:22:02 -0700

    add some better config to neotree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,6 @@ require("config.lazy")
 -- TODO: make tab names more intuitive
 -- TODO: make sure DAP works correctly and highlight groups are all good
 -- TODO: learn how to do all the actions i normally do within vscode github using lazygit
--- TODO: dap ui should not have all the panes - we just want variables, stack, and
 -- TODO: use my fork of telescope
 -- TODO: cycle through telescope history: https://www.reddit.com/r/neovim/comments/phndpv/can_telescope_remember_my_last_search_result/
 -- TODO: check out file at specific commit using telescope
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -58,6 +58,20 @@ return {
       }
     end,
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
       close_if_last_window = true,
       default_component_configs = {
         git_status = {
