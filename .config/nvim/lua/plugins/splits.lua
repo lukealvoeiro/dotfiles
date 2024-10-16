2ac6ce41cbb395138375daf92219f8469d06b16b
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-10-15 17:05:03 -0700

    handle smart splits inside of wezterm

---
+++ .config/nvim/lua/plugins/splits.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mrjones2014/smart-splits.nvim",
+    keys = {
+      { "<C-h>", "<cmd>SmartCursorMoveLeft<CR>", desc = "Move cursor left pane" },
+      { "<C-l>", "<cmd>SmartCursorMoveRight<CR>", desc = "Move cursor right pane" },
+      { "<C-k>", "<cmd>SmartCursorMoveUp<CR>", desc = "Move cursor up pane" },
+      { "<C-j>", "<cmd>SmartCursorMoveDown<CR>", desc = "Move cursor down pane" },
+    },
+  },
+}
--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,54 @@
 local wezterm = require("wezterm")
 local config = {}
 
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
+-- you have to use this instead, but note that this will not work
+-- in all cases (e.g. over an SSH connection). Also note that
+-- `pane:get_foreground_process_name()` can have high and highly variable
+-- latency, so the other implementation of `is_vim()` will be more
+-- performant as well.
+local function is_vim(pane)
+	-- This gsub is equivalent to POSIX basename(3)
+	-- Given "/foo/bar" returns "bar"
+	-- Given "c:\\foo\\bar" returns "bar"
+	local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+	return process_name == "nvim" or process_name == "vim"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 config.color_scheme = "Ayu Mirage"
 config.window_background_opacity = 0.92
 config.font = wezterm.font("ZedMono Nerd Font")
@@ -20,6 +68,10 @@ config.window_frame = {
 	-- Default to 10.0 on Windows but 12.0 on other systems
 	font_size = 13.0,
 }
+config.inactive_pane_hsb = {
+	saturation = 0.95,
+	brightness = 0.6,
+}
 
 config.keys = {
 	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
@@ -36,6 +88,11 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action({ SendString = "\x1bOF" }),
 	},
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
 }
 -- config.macos_force_enable_shadow = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
